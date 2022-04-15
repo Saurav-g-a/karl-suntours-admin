84559bcf178ae3dd4dae6a0af0988451c19686eb
PoonamCodenomad <46337636+PoonamCodenomad@users.noreply.github.com>
2022-04-15 20:37:08 +0530

    changes

--- src/app/services/dbio.service.ts
+++ src/app/services/dbio.service.ts
@@ -7,123 +7,125 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 })
 export class DbioService {
 
-  constructor(private http : HttpClient) { }
-userLogin(data){
-  return this.http.post(environment.url+"/auth/login",data)
-}
-deleteHomepageSection(id){
-  return this.http.delete(environment.url+`/homepage/deleteHomePageSection/${id}`)
-}
-deleteDestinationpageSection(destinationId,sectionId){
-  return this.http.delete(environment.url+`/destination/deleteDestinationSection/${destinationId}/${sectionId}`,)
-}
-uploadImage(image){
-  console.log(image)
-  return this.http.post(environment.url+"/upload/images",image)
-}
-homePageMeta(data){
-  return this.http.post(environment.url+"/pagemeta/updateMeta",data)
-}
-
-  createHomepageSection(section){
-    return this.http.post(environment.url+"/homepage/createSection",section)
+  constructor(private http: HttpClient) { }
+  userLogin(data) {
+    return this.http.post(environment.url + "/auth/login", data)
   }
-  getUserDetails(id){
-    return this.http.get(environment.url+"/users/"+id)
+  deleteHomepageSection(id) {
+    return this.http.delete(environment.url + `/homepage/deleteHomePageSection/${id}`)
   }
-   createUser(data){
-  return this.http.post(environment.url+"/users",data)
-}
-getUser(){
-  return this.http.get(environment.url+"/users")
-}
-updateUserData(data){
-  return this.http.post(environment.url+"/users/update",data)
-}
-  getHomepageSection(id){
-    return this.http.get(environment.url+"/homepage/section/"+id)
+  deleteHolidayPageSection(id) {
+    return this.http.delete(environment.url + `/holiday/deleteholidayPageSection/${id}`)
   }
- 
-  updateHomepageSection(data){
-    return this.http.post(environment.url+"/homepage/updateSection",data)
+  deleteDestinationpageSection(destinationId, sectionId) {
+    return this.http.delete(environment.url + `/destination/deleteDestinationSection/${destinationId}/${sectionId}`,)
   }
-  getHomePage(){
-      return this.http.get(environment.url+"/homepage/sections")
+  uploadImage(image) {
+    console.log(image)
+    return this.http.post(environment.url + "/upload/images", image)
+  }
+  homePageMeta(data) {
+    return this.http.post(environment.url + "/pagemeta/updateMeta", data)
   }
 
+  createHomepageSection(section) {
+    return this.http.post(environment.url + "/homepage/createSection", section)
+  }
+  getUserDetails(id) {
+    return this.http.get(environment.url + "/users/" + id)
+  }
+  createUser(data) {
+    return this.http.post(environment.url + "/users", data)
+  }
+  getUser() {
+    return this.http.get(environment.url + "/users")
+  }
+  updateUserData(data) {
+    return this.http.post(environment.url + "/users/update", data)
+  }
+  getHomepageSection(id) {
+    return this.http.get(environment.url + "/homepage/section/" + id)
+  }
 
-  getPageMeta(type){
-    return this.http.get(environment.url+"/pagemeta/"+type)
+  updateHomepageSection(data) {
+    return this.http.post(environment.url + "/homepage/updateSection", data)
   }
-  getDestinationMetaData(id)
-  {
-    return this.http.get(environment.url+'/destination/destinationMeta/'+id)
-  }
-  updateDestinationMetaData(data){
-    return this.http.post(environment.url+"/destination/update",data)
-  }
- updateMetaData(meta){
-    return this.http.post(environment.url+"/pagemeta/updateMeta",meta)
-  }
-
-  createMetaData(meta){
-    return this.http.post(environment.url+"/pagemeta/createMeta",meta)
-  }
-  getAllMeta(){
-    return this.http.get(environment.url+"/pagemeta/list")
+  getHomePage() {
+    return this.http.get(environment.url + "/homepage/sections")
   }
 
 
+  getPageMeta(type) {
+    return this.http.get(environment.url + "/pagemeta/" + type)
+  }
+  getDestinationMetaData(id) {
+    return this.http.get(environment.url + '/destination/destinationMeta/' + id)
+  }
+  updateDestinationMetaData(data) {
+    return this.http.post(environment.url + "/destination/update", data)
+  }
+  updateMetaData(meta) {
+    return this.http.post(environment.url + "/pagemeta/updateMeta", meta)
+  }
 
-  createDestination(section){
-    return this.http.post(environment.url+"/destination/create",section)
+  createMetaData(meta) {
+    return this.http.post(environment.url + "/pagemeta/createMeta", meta)
   }
-  getDestination(id){
-    return this.http.get(environment.url+"/destination/"+id)
-  }
-  updateDestination(data){
-    return this.http.post(environment.url+"/destination/update",data)
-  }
-  getAllDestinations(){
-      return this.http.get(environment.url+"/destination/list")
-  }
-  createDestinationSection(data){
-    return this.http.post(environment.url+"/destination/addsection",data)
-  }
-  getDestinationSections(data){
-    return this.http.get(environment.url+"/destination/"+data+"/sections")
-  }
-  getDestinationSection(data){
-    return this.http.get(environment.url+"/destination/section/"+data)
-  }
-  updateDestinationSection(data){
-    return this.http.post(environment.url+"/destination/updateSection/",data)
+  getAllMeta() {
+    return this.http.get(environment.url + "/pagemeta/list")
   }
 
 
 
-  createHoliday(section){
-    return this.http.post(environment.url+"/holiday/create",section)
+  createDestination(section) {
+    return this.http.post(environment.url + "/destination/create", section)
   }
-  getHoliday(id){
-    return this.http.get(environment.url+"/holiday/"+id)
+  getDestination(id) {
+    return this.http.get(environment.url + "/destination/" + id)
   }
-  updateHoliday(data){
-    return this.http.post(environment.url+"/holiday/update",data)
+  updateDestination(data) {
+    return this.http.post(environment.url + "/destination/update", data)
   }
-  getAllHolidays(){
-      return this.http.get(environment.url+"/holiday/list")
+  getAllDestinations() {
+    return this.http.get(environment.url + "/destination/list")
   }
-  createHolidaySection(data){
-    return this.http.post(environment.url+"/holiday/addsection",data)
+  createDestinationSection(data) {
+    return this.http.post(environment.url + "/destination/addsection", data)
   }
-  getHolidaySections(data){
-    return this.http.get(environment.url+"/holiday/"+data+"/sections")
+  getDestinationSections(data) {
+    return this.http.get(environment.url + "/destination/" + data + "/sections")
   }
-  getHolidaySection(data){
-    return this.http.get(environment.url+"/holiday/section/"+data)
+  getDestinationSection(data) {
+    return this.http.get(environment.url + "/destination/section/" + data)
   }
-  updateHolidaySection(data){
-    return this.http.post(environment.url+"/holiday/updateSection/",data)
+  updateDestinationSection(data) {
+    return this.http.post(environment.url + "/destination/updateSection/", data)
+  }
+
+
+
+  createHoliday(section) {
+    return this.http.post(environment.url + "/holiday/create", section)
+  }
+  getHoliday(id) {
+    return this.http.get(environment.url + "/holiday/" + id)
+  }
+  updateHoliday(data) {
+    return this.http.post(environment.url + "/holiday/update", data)
+  }
+  getAllHolidays() {
+    return this.http.get(environment.url + "/holiday/list")
+  }
+  createHolidaySection(data) {
+    return this.http.post(environment.url + "/holiday/addsection", data)
+  }
+  getHolidaySections(data) {
+    return this.http.get(environment.url + "/holiday/" + data + "/sections")
+  }
+  getHolidaySection(data) {
+    return this.http.get(environment.url + "/holiday/section/" + data)
+  }
+  updateHolidaySection(data) {
+    return this.http.post(environment.url + "/holiday/updateSection/", data)
   }
 }
--- src/app/views/holiday/holiday-section-list/holiday-section-list.component.ts
+++ src/app/views/holiday/holiday-section-list/holiday-section-list.component.ts
@@ -9,24 +9,36 @@ import { DbioService } from 'src/app/services/dbio.service';
 })
 export class HolidaySectionListComponent implements OnInit {
 
-  sections:any=[]
-  holidayId=''
+  sections: any = []
+  holidayId = ''
   constructor(
-    private dbioService:DbioService,
-    private activatedRoute : ActivatedRoute, 
-    private router:Router
+    private dbioService: DbioService,
+    private activatedRoute: ActivatedRoute,
+    private router: Router
   ) { }
 
   ngOnInit(): void {
-    this.holidayId =  this.activatedRoute.snapshot.params.id
-     this.dbioService.getHolidaySections(this.holidayId).subscribe(res=>[
-      this.sections =res
-     ])
+    this.holidayId = this.activatedRoute.snapshot.params.id
+    this.sectionList()
   }
-  edit(id){
-    this.router.navigate(['holiday/'+this.holidayId+'/section/'+id])
+  sectionList() {
+    this.dbioService.getHolidaySections(this.holidayId).subscribe(res => [
+      this.sections = res
+    ])
   }
-  add(){
-    this.router.navigate(['holiday/create-holiday-section/'+this.holidayId+'/'])
+  edit(id) {
+    this.router.navigate(['holiday/' + this.holidayId + '/section/' + id])
+  }
+  add() {
+    this.router.navigate(['holiday/create-holiday-section/' + this.holidayId + '/'])
+  }
+  delete(id) {
+    this.dbioService.deleteHolidayPageSection(id).subscribe((res:any)=>{
+      console.log(res)
+      if (res){
+        this.sectionList()
+      }
+    })
+ 
   }
 }
